031e139e7c3343f20578bf61c3616ea4bf59f49b
danajackson2 <danajackson2@gmail.com>
2021-02-15 16:30:30 -0800

    datetime format

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -6,7 +6,7 @@ class PapersController < ApplicationController
 
     def create
         # hook up current user
-        paper = Paper.create(title: paper_params['title'], date: DateTime.now.strftime('%F'), user_id: User.first.id)
+        paper = Paper.create(title: paper_params['title'], date: DateTime.now.strftime('%b %d, %Y'), user_id: User.first.id)
         paper_params['articles'].each do |a| 
             art = Article.find_or_create_by(title: a['title'], author: a['author'], description: a['description'], content: a['content'], url: a['url'], img_url: a['urlToImage'])
             PaperArticleJoin.create(paper_id: paper.id, article_id: art.id)
