b114bd977d829c7c029c14094a580f94fd426af3
danajackson2 <danajackson2@gmail.com>
2021-02-16 11:58:43 -0800

    login signup

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,4 +1,5 @@
 class PapersController < ApplicationController
+    skip_before_action :authorized, only: [:index]
 
     def index
         render json: Paper.all, include: [:articles, :user]
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :paper_article_joins
   resources :articles
   resources :papers, only: [:create, :index]
-  resources :users
+  resources :users, only: [:create, :destroy]
+  post '/signup', to: 'users#create'
+  post '/login', to: 'auth#create'
+  get '/persist', to: 'auth#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
