abc14dc344c60a943d768afc49d2c297a7c76942
danajackson2 <danajackson2@gmail.com>
2021-02-20 10:34:19 -0800

    cors take 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://news-choose.herokuapp.com/'
 
-    resource 'https://news-choose-backend.herokuapp.com/*',
+    resource 'https://news-choose-backend.herokuapp.com',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
